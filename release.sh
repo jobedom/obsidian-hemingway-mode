ead0c022f7b971031ebf3c2669303aca3691436b
Joaquín Bernal <jobedom@gmail.com>
2024-03-23 20:54:02 +0100

    Simplify release script even more

--- release.sh
+++ release.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-MINIMUM_OBSIDIAN_VERSION="0.15.0"
-
 set -euo pipefail
 
 if [[ $(git status --porcelain) ]]; then
@@ -12,22 +10,8 @@ fi
 npm version patch
 NEW_VERSION=$(jq ".version" package.json)
 
-echo "Updating to version ${NEW_VERSION}"
-echo "with minimum Obsidian version ${MINIMUM_OBSIDIAN_VERSION}"
-
-echo "Updating manifest.json"
-TEMP_FILE=$(mktemp)
-jq ".version |= \"${NEW_VERSION}\" | .minAppVersion |= \"${MINIMUM_OBSIDIAN_VERSION}\"" manifest.json > "$TEMP_FILE" || exit 1
-mv "$TEMP_FILE" manifest.json
-
-echo "Updating versions.json"
-TEMP_FILE=$(mktemp)
-jq ". += {\"${NEW_VERSION}\": \"${MINIMUM_OBSIDIAN_VERSION}\"}" versions.json > "$TEMP_FILE" || exit 1
-mv "$TEMP_FILE" versions.json
-
-echo "Creating git commit, tag, and pushing"
 git add -A .
-git commit -m"Update to version ${NEW_VERSION}"
-git tag "${NEW_VERSION}"
+git commit -m "Update to version ${NEW_VERSION}"
 git push
-LEFTHOOK=0 git push --tags
+git tag "${NEW_VERSION}"
+git push --tags
